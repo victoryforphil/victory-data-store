14b3c6f4affe66bdd1d8f33311bd7dbbec16b82e
Alex Carter <victoryforphil@gmail.com>
2024-10-18 01:04:28 -0700

    Got memory down to 12mb

---
+++ benches/serde_tracy.rs
@@ -0,0 +1,35 @@
+use divan::counter::{BytesCount, CharsCount, ItemsCount};
+use divan::AllocProfiler;
+use tracing_tracy::client::ProfiledAllocator;
+use std::collections::*;
+use tracing_subscriber::layer::SubscriberExt;
+use victory_data_store::{primitives::serde::serialize::to_map, test_util::BigState};
+#[global_allocator]
+static GLOBAL: ProfiledAllocator<std::alloc::System> =
+    ProfiledAllocator::new(std::alloc::System, 100);
+fn main() {
+    // Run registered benchmarks.
+
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::registry().with(tracing_tracy::TracyLayer::default()),
+    )
+    .expect("setup tracy layer");
+    divan::main();
+}
+
+#[divan::bench]
+fn bench_to_map_rate(bencher: divan::Bencher) {
+    let len: usize = 10;
+
+    bencher
+        .with_inputs(|| -> Vec<BigState> { vec![BigState::new(); len] })
+        .input_counter(|s: &Vec<BigState>| {
+            // Changes based on input.
+            BytesCount::of_iter(s.iter())
+        })
+        .input_counter(|s: &Vec<BigState>| {
+            // Changes based on input.
+            ItemsCount::of_iter(s.iter())
+        })
+        .bench_refs(|s: &mut Vec<BigState>| to_map(s).unwrap());
+}
